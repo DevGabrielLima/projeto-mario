662882479b7c0ba772609f8a8169f5c2a55f5e5c
Gabriel Lima <gabriellima.costa@hotmail.com>
2023-01-24 21:37:16 -0300

    alteração

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+console.log(document.querySelector(".botao-trailer"));
+
+botaoTrailer.addEventListener("click", () => {
+  modal.classList.add("aberto");
+  video.setAttribute("src", linkDoVideo);
+});
+
+const botaoFecharModal = document.querySelector(".fechar-modal");
+
+botaoFecharModal.addEventListener("click", () => {
+  modal.classList.remove("aberto");
+  video.setAttribute("src", "");
+});
